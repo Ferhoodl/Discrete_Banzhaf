c366d2490c7c82b59dda31843b32db0d4a3aa1e1
IWSteele <81664061+IWSteele@users.noreply.github.com>
2022-07-24 23:32:41 -0500

    Removed imports in Banzahf

--- Banzahf.java
+++ Banzahf.java
@@ -1,6 +1,3 @@
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
 import java.util.ArrayList; // Let our program use arraylists
 
 /*
