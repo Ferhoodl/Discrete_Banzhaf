6ed651139c8a5acbc61df8f6ce9cf31e4780be14
Claire H <50155045+clairejhaas@users.noreply.github.com>
2022-08-25 19:56:02 -0500

    Elaborated on Banzhaf terminology

--- Banzhaf.java
+++ Banzhaf.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList; // Lets our program use arraylists
 
+//The Banzhaf Ratio is the computed Banzhaf Value divided by the number of electoral votes allotted to each state.
+
 /*
 Objective: This is the main class. It tells the Parser class to turn a text file of states and electoral votes
-into an arraylist of State objects with names and votes values filled in, but Banzhaf Power Index values
+into an arraylist of State objects with names and votes values filled in, but Banzhaf Ratio values
 missing. Then, the arraylist is sent to the PowerAlgo class, where our algorith to approximate the Banzhaf
 values is applied. An arraylist with each state's power index value is filled in, and the values are printed
 to the console.
@@ -19,7 +21,7 @@ public class Banzhaf{
       
       // print out the states and power indexes
 
-      System.out.println("The Banzhaf Power Index of each state is as follows:");
+      System.out.println("The Banzhaf Ratio of each state is as follows:");
       for(State state : states){
          System.out.println(state.getName() + ": " + state.getIndex());
       }
